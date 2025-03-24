cee34f4ac1d5233249e8a535003597d8d3bbbaf8
Pontusp01 <pontusp01j@gmail.com>
2025-03-24 10:02:08 +0100

    add in cert

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,16 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
+import fs from "fs";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
     port: 8080,
+    https: {
+      key: fs.readFileSync('C:/dev/cert/key_decrypted.pem'),  // Använd key_decrypted.pem om du tog bort lösenordet
+      cert: fs.readFileSync('C:/dev/cert/cert.pem'),
+    },
   },
   plugins: [
     react(),
